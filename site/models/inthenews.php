8444b23de3c2f66a384a9e80e0b63147a67cf0d2
webmaster-cepr <webmaster@cepr.net>
2013-08-27 12:21:22 -0400

    Hello World test

--- site/models/inthenews.php
+++ site/models/inthenews.php
@@ -1,60 +1,46 @@
 <?php
-
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
-
-// import Joomla model item library
+ 
+// import Joomla modelitem library
 jimport('joomla.application.component.modelitem');
-
+ 
 /**
  * InTheNews Model
  */
-
- class InTheNewsModelInTheNews extends JModelItem
- {
-
- 	/**
-	 * @var string msg
-	 */
-
-	 protected $msg;
-
-	/**
-	 * Get the message
-	 * @return string The message will be displayed to the user
-	 */
-
-	 public function getMsg()
-	 {
-	 	if(!isset($this->msg))
-		{
-
-			//Uses JInput if magic quotes is turned off. Falls back to use JRequest
-			if(!get_magic_quotes_gpc()) {
-				$id = JFactory::getApplication()->input->get('id', 1, 'INT' );
-			}
-
-			else {
-				$id = JRequest::getInt('id');
-			}
-
-			switch ($id) {
-
-			case 2:
-			$this->msg = "Why isn't Joomla doing this for me?";
-			break;
-
-			default:
-			case 1:
-			$this->msg = "Stupid Joomla";
-			break;
-			
-			}
-
-		}
-
-
-		return $this->msg;
-	}
-
+class InTheNewsModelInTheNews extends JModelItem
+{
+        /**
+         * @var string msg
+         */
+        protected $msg;
+ 
+        /**
+         * Get the message
+         * @return string The message to be displayed to the user
+         */
+        public function getMsg() 
+        {
+                if (!isset($this->msg)) 
+                {
+                        //Uses JInput if magic quotes is turned off. Falls back to use JRequest.
+                        if(!get_magic_quotes_gpc()) {
+                                $id = JFactory::getApplication()->input->get('id', 1, 'INT' );
+                        } else {
+                                $id = JRequest::getInt('id');
+                        }
+ 
+                        switch ($id) 
+                        {
+                        case 2:
+                                $this->msg = 'Good bye World!';
+                        break;
+                        default:
+                        case 1:
+                                $this->msg = 'Hello World!';
+                        break;
+                        }
+                }
+                return $this->msg;
+        }
 }
